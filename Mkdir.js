551721ead48a51f06ff5ad1faca661b50fd9fac5
devlopernabin <77477530+devlopernabin@users.noreply.github.com>
2021-01-21 22:29:31 +0545

    Update Mkdir.js

--- Mkdir.js
+++ Mkdir.js
@@ -1,9 +1,10 @@
 const fs=require('fs')
 
- fs.Mkdir('Main', ,(err)=>{
+ fs.Mkdir('Main', (err)=>{
 
         if (err)
         console.log(err)
         else
         console.log('successfully created the folder!')
     })
+    
